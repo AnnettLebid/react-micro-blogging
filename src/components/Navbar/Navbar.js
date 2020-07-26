b80f3de53c25fcabc3ac4bde85c9e7e32fcef1bc
Anna <annalez16@gmail.com>
2020-07-26 23:42:29 +0300

    Add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className = "navbar">
+             <Router>
+                <Link className="home-menu" to="/">Home</Link>
+                <Link className="profile-menu" to="/profile">Profile</Link>
+            </Router>            
         </div>
     )
 }
